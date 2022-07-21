a8aea10954f1710eed2b5b8bfdb55e0271bed47a
adedokun moses <adedokunmoses.1450@gmail.com>
2022-07-21 18:15:12 +0100

    new update

--- src/views/login.vue
+++ src/views/login.vue
@@ -2,16 +2,14 @@
   <div class="form-page">
     <h5>Logo</h5>
 
-    <form class="regform"  @submit.prevent="nextPage()">
+    <form class="regform" @submit.prevent="nextPage()">
       <h5>Register With Us</h5>
       <h3>Your are just a few clicks away from using our services</h3>
 
       <div class="namefield">
         <div class="formgroup">
           <label>First Name </label><br />
-          <input
-            type="text"
-            placeholder="Enter Your First Name" />
+          <input type="text" placeholder="Enter Your First Name" />
         </div>
         <div class="formgroup">
           <label>Last Name </label><br />
@@ -21,17 +19,13 @@
 
       <div class="formgroup_">
         <label>Email Address </label><br />
-        <input
-          type="email"
-          placeholder="adedokunmoses@gmail.com"
-       
-        />
+        <input type="email" placeholder="adedokunmoses@gmail.com" required/>
       </div>
-      
+
       <div class="formgroup_">
-            <vue-tel-input v-model="phone" class="formgroup_1"></vue-tel-input>
+        <vue-tel-input v-model="phone" class="formgroup_1"></vue-tel-input>
       </div>
-     
+
       <div class="formgroup_">
         <label>Gender </label><br />
         <select>
@@ -42,10 +36,10 @@
         </select>
       </div>
       <div class="formgroup_">
-        <button >Register</button>
+        <button>Register</button>
         <h4>
           Already have an account
-          <span><router-link to='#'>Sign Up Here</router-link></span>
+          <span><router-link to="#">Sign Up Here</router-link></span>
         </h4>
       </div>
     </form>
@@ -53,23 +47,20 @@
 </template>
 
 <script>
-
-  import { VueTelInput } from 'vue-tel-input';
+import { VueTelInput } from "vue-tel-input";
 export default {
-  components: {VueTelInput},
-  data(){
-return{
-  phone:''
-}
+  components: { VueTelInput },
+  data() {
+    return {};
   },
- methods: {
+  methods: {
+    nextPage() {
+      //alert("Input correct details");
 
-  nextPage(){
-    alert('Welcome')
-    this.$router.push('/congratulatory');
-  }
- }
-}
+      this.$router.push("/congratulatory");
+    },
+  },
+};
 </script>
 
 
@@ -84,6 +75,7 @@ return{
 .regform {
   width: 50%;
   margin: auto;
+  padding-bottom: 10px;
 }
 .regform h5 {
   color: #1244a5;
@@ -117,7 +109,7 @@ return{
   border: 1px solid #a3a6ad;
   border-radius: 8px;
 }
-.formgroup input:focus{
+.formgroup input:focus {
   background: #a3a6ad;
   transition: ease-in 2s background;
 }
@@ -131,7 +123,7 @@ return{
   border: 1px solid #a3a6ad;
   border-radius: 8px;
 }
-.formgroup_ input:focus{
+.formgroup_ input:focus {
   background: #a3a6ad;
   transition: ease-in 2s background;
 }
@@ -178,56 +170,50 @@ return{
   text-decoration: none;
 }
 
-.formgroup_1{
+.formgroup_1 {
   width: 95%;
   background: #ffffff;
   padding: 10px 10px;
   margin: 10px 0px;
-  outline: none!important;
-  border: none !important;;
+  outline: none !important;
+  border: none !important;
   border: 1px solid #a3a6ad;
   border-radius: 8px;
 }
 
-
-
-@media only screen and (max-width: 600px){
+@media only screen and (max-width: 600px) {
   .regform {
-  width: 90%;
-  margin: auto;
-}
-.form-page h5 {
-  padding: 10px 10px;
-  font-size: 40px;
-  text-align: center;
-}
+    width: 90%;
+    margin: auto;
+  }
+  .form-page h5 {
+    padding: 10px 10px;
+    font-size: 40px;
+    text-align: center;
+  }
 
+  .regform h5 {
+    font-size: 20px;
+  }
 
-.regform h5 {
-  font-size: 20px;
-}
+  .regform h3 {
+    text-align: center;
+    font-size: 10px;
+    color: #61666d;
+  }
+  .namefield {
+    display: block;
+  }
+  .formgroup_ input {
+    width: 90%;
+  }
 
-.regform h3 {
-  text-align: center;
-  font-size: 10px;
-  color: #61666d;
-}
-.namefield {
-  display: block;
-}
-.formgroup_ input{
-  width: 90%;
-}
-
-.formgroup_1{
-  width: 90%;
-
-}
-.formgroup_ h4 {
-  text-align: left;
-  margin: 10px 0px;
-}
-
-
+  .formgroup_1 {
+    width: 90%;
+  }
+  .formgroup_ h4 {
+    text-align: left;
+    margin: 10px 0px;
+  }
 }
 </style>
